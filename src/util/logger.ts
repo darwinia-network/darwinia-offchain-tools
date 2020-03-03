82ce53b15744eb470262f82c8a77af3ba219ff81
WoeOm <1015996366@qq.com>
2020-03-03 22:09:49 +0800

    format console log

--- src/util/logger.ts
+++ src/util/logger.ts
@@ -3,9 +3,14 @@ import winston from "winston";
 const options: winston.LoggerOptions = {
     transports: [
         new winston.transports.Console({
-            level: process.env.NODE_ENV === "production" ? "error" : "debug"
+            level: "debug",
+            format: winston.format.combine(
+                winston.format.colorize(),
+                winston.format.simple()
+            )
         }),
-        new winston.transports.File({ filename: "debug.log", level: "debug" })
+        new winston.transports.File({ filename: "debug.log", level: "debug" }),
+
     ]
 };
 
